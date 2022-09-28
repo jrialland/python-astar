8c102c21700832572be77725f49833b77ab69174
Julien Rialland <julien.rialland@altran.com>
2022-09-28 17:52:54 +0200

    use poetry for releasing

--- release.sh
+++ release.sh
@@ -1,5 +1,23 @@
+#! /bin/bash
+set -euo pipefail
+
+thisfile=$(readlink -m "$0")
+thisdir=$(dirname "$thisfile")
+pushd "$thisdir"
+
+if [ ! -d .venv ]; then
+    python -m venv .venv
+fi
+
+source .venv/bin/activate
+pip install poetry
 
 
+#some checks
+poetry run mypy astar # check types
+poetry run pytest # unit tests
+
+#configure pypi
 cat > ~/.pypirc << '_eof'
 [distutils]
 index-servers = pypi
@@ -13,10 +31,11 @@ repository: https://test.pypi.org/legacy/
 username: jrialland
 _eof
 
+# delete on exit
+trap "rm ~/.pypirc" EXIT
+
 rm ./dist -rf
+poetry build
+poetry run twine upload dist/*.whl
 
-python2 setup.py sdist bdist_wheel
-python3 setup.py sdist bdist_wheel
-
-twine upload dist/*.whl
-
+popd
