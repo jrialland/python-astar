e6d3357abd1a5964a949f65b4d261451aee2b1f3
Julien Rialland <julien.rialland@altran.com>
2023-06-16 10:04:22 +0200

    test case that reproduces #15

--- tests/basic/test_basic.py
+++ tests/basic/test_basic.py
@@ -1,6 +1,24 @@
 import unittest
-import astar
+from astar import AStar
 
+class BasicAStar(AStar):
+    def __init__(self, nodes):
+        self.nodes = nodes
+
+    def neighbors(self, n):
+        for n1, d in self.nodes[n]:
+            yield n1
+
+    def distance_between(self, n1, n2):
+        for n, d in self.nodes[n1]:
+            if n == n2:
+                return d
+            
+    def heuristic_cost_estimate(self, current, goal):
+        return 1
+    
+    def is_goal_reached(self, current, goal):
+        return current == goal
 
 class BasicTests(unittest.TestCase):
 
@@ -13,22 +31,28 @@ class BasicTests(unittest.TestCase):
                  'C': [('D', 20)], 
                  'D': [('B', 20)]}
 
-        def neighbors(n):
-            for n1, d in nodes[n]:
-                yield n1
+        path = BasicAStar(nodes).astar('A', 'B')
+        self.assertIsNotNone(path)
+        if path:
+            path = list(path)
+            self.assertEqual(4, len(path))
+            for i, n in enumerate('ACDB'):
+                self.assertEqual(n, path[i])
 
-        def distance(n1, n2):
-            for n, d in nodes[n1]:
-                if n == n2:
-                    return d
-
-        def cost(n, goal):
-            return 1
-        path = list(astar.find_path('A', 'B', neighbors_fnct=neighbors,
-                    heuristic_cost_estimate_fnct=cost, distance_between_fnct=distance))
-        self.assertEqual(4, len(path))
-        for i, n in enumerate('ACDB'):
-            self.assertEqual(n, path[i])
+    def test_issue_15(self):
+        """This test case reproduces https://github.com/jrialland/python-astar/issues/15.
+        B has no neighbors, therefore the computation should return None and not raise an exception.
+        """
+        node = {
+            'A': [('B', 200000)],
+            'C': [('D', 200000)],
+            'D': [('E', 200000)],
+            'E': [('F', 200000)],
+            'B': [],
+            'F': []
+        }
+        path = BasicAStar(node).astar('A', 'D')
+        self.assertIsNone(path)
 
 if __name__ == '__main__':
     unittest.main()
